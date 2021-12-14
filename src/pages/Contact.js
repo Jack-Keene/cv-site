82dcd92d98ee8dfece6457e54ca086e8b52397a3
Jack-Keene <jack.keene1@gmail.com>
2021-12-14 16:41:25 +0000

    form button

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,11 +10,12 @@ function Contact() {
         <section className="section contact" id="contact" >
             <Background className={"background brightness"} image={Image}/>
             <h1 className="header"> Contact</h1>
-            <form className="form" name="contact" method="POST" data-netlify="true" netlify netlify-honeypot="bot-field">
+            <form className="form" name="contact" method="POST" data-netlify="true">
                 <input type="hidden" name="form-name" value="contact" />
                 <input autoComplete="off" type="text" id="name" name="name" required placeholder="NAME" />
                 <input autoComplete="off" type="email" id="email" name="email" required placeholder="EMAIL"/>
                 <textarea autoComplete="off" type="textarea" id="message" name="message" required placeholder="MESSAGE"/>
+                <button className="form"type="submit">Send</button>
             </form>
         </section>
     );
