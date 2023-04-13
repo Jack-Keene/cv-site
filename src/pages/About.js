25a8e050080e9bbf035e11f5abdb3c10e55cf1b8
Jack Keene <jack.keene@snowplowanalytics.com>
2023-04-13 12:38:57 +0100

    Update personal statement

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,28 +7,28 @@ import Rotate from '../components/Rotate';
 
 
 function About() {
-    return (       
+    return (
         <section id="about" className="section about">
-            <Rotate text="ABOUT ME"/>
+            <Rotate text="ABOUT ME" />
             <div className='about-info'>
                 <FontAwesomeIcon className="icon svg-1" icon={faCrosshairs} size="2x" />
                 <h2 className='about-head'>about me.</h2>
                 <p className="p-1">
-                    Since graduating from university in 2017, I have worked for Nissan Motors GB as an analyst and more recently as a performance and operations manager. I began learning Python to be more efficient in my work and I've not stopped developing my programming skills since.                
+                    A software engineer currently working at Snowplow.io, helping people create behavioural data for their products.
                 </p>
                 <p className="p-2">
-                    In November 2021 I left the automotive industry to pursue software development full time and I'm currently looking for a new role. Feel free to contact me directly or through the form on this site.
+                    In November 2021 I left a career in the automotive industry to pursue software development full time. Feel free to contact me directly or through the form on this site.
                 </p>
             </div>
-            
+
             <div className='skills' >
                 <h2 className='about-skills'>skills.</h2>
-                <ul  className='skills-list'>
+                <ul className='skills-list'>
                     <li>Python</li>
+                    <li>ReactJS</li>
                     <li>Java</li>
                     <li>HTML</li>
                     <li>CSS</li>
-                    <li>ReactJS</li>
                 </ul>
             </div>
         </section>
