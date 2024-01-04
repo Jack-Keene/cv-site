606bfac6b7ab786f2fc9632222e5d7695e1fe00e
Jack Keene <jack.keene@snowplowanalytics.com>
2024-01-04 18:28:09 +0000

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
 'use client'
 import LandingPage from '../components/Landing/LandingPage'
 import Navbar from '../components/Navbar/Navbar'
-import SpotifyWidget from '../components/Spotify/SpotifyWidget'
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { muiTheme } from '../theme';
 import { CssBaseline } from "@mui/material";
-import { IBM_Plex_Mono } from "next/font/google";
-import { Rubik } from "next/font/google";
 import styles from './page.module.css'
 
 const theme = createTheme(muiTheme);
