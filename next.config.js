fa3917dac4d0106d3dc39f4c3b7bd25c04163ac5
Jack Keene <jack.keene@snowplowanalytics.com>
2024-01-04 18:34:38 +0000

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async headers() {
+      return [
+        {
+          source: '/about',
+          headers: [
+            {
+              key: 'x-custom-header',
+              value: 'my custom header value',
+            },
+            {
+              key: 'x-another-custom-header',
+              value: 'my other custom header value',
+            },
+          ],
+        },
+      ]
+    },
+  }
+  
