a83808fb0708b2e738b49974dda903b6cc3e2947
Jack Keene <jack.keene@snowplowanalytics.com>
2024-01-04 19:05:04 +0000

    Update api address

--- src/hooks/spotify.ts
+++ src/hooks/spotify.ts
@@ -5,7 +5,7 @@ const fetcher = (...args: any) => fetch(...args).then((res) => res.json());
 
 export function useNowPlaying() {
     const { data, error, isLoading } = useSWR(
-        "http://localhost:3000/api/now-playing",
+        "/api/now-playing",
         fetcher,
         { refreshInterval: 3000 }
     );
