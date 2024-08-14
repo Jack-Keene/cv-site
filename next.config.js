492270a615467d772588aa92b465f874565e9c9e
Jack Keene <jack.keene@snowplowanalytics.com>
2024-08-14 15:34:25 +0100

    update configs

--- next.config.js
+++ next.config.js
@@ -1,22 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    async headers() {
-      return [
-        {
-          target: 'serverless',
-          source: '/about',
-          headers: [
-            {
-              key: 'x-custom-header',
-              value: 'my custom header value',
-            },
-            {
-              key: 'x-another-custom-header',
-              value: 'my other custom header value',
-            },
-          ],
-        },
-      ]
-    },
-  }
-  
+  // Target must be serverless
+  target: 'serverless'
+};
