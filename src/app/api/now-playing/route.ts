3e5cf81cfa21b6db20de66da2d3ba433f85e07fc
Jack Keene <jack.keene@snowplowanalytics.com>
2024-08-14 12:56:37 +0100

    add no cache to next api

--- src/app/api/now-playing/route.ts
+++ src/app/api/now-playing/route.ts
@@ -3,5 +3,8 @@ import { getNowPlaying, getNowPlayingItem } from '../../../lib/spotify';
 
 export async function GET() {
     const response = await getNowPlayingItem();
+    const res = NextResponse.json(response);
+    res.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    return res
     return NextResponse.json(response)
 }
