a2f695af97bc85605f4c8c972fe562a3971c689a
Jack Keene <jack.keene@snowplowanalytics.com>
2025-07-10 17:14:32 +0100

    update webpack config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,32 @@
 /** @type {import('next').NextConfig} */
+const Dotenv = require('dotenv-webpack');
+
+/** @type {import('next').NextConfig} */
 module.exports = {
-    async headers() {
-      return [
-        {
-          source: '/about',
-          headers: [
-            {
-              key: 'x-custom-header',
-              value: 'my custom header value',
-            },
-            {
-              key: 'x-another-custom-header',
-              value: 'my other custom header value',
-            },
-          ],
-        },
-      ]
-    },
-  }
-  
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'x-custom-header',
+            value: 'my custom header value',
+          },
+          {
+            key: 'x-another-custom-header',
+            value: 'my other custom header value',
+          },
+        ],
+      },
+    ]
+  },
+  webpack: (config) => {
+    config.plugins.push(
+      new Dotenv({
+        systemvars: true, // Load system environment variables as well
+      })
+    );
+    return config;
+  },
+}
+
